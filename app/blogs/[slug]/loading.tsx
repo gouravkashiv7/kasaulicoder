21a58676f8affd6ec28878af4076f1978f7d9b63
Gourav Kashiv <gouravkashiv3@gmail.com>
2026-03-05 11:37:31 +0530

    ui optimizations

---
+++ app/blogs/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+import PageLoading from "@/components/ui/page-loading";
+
+export default function Loading() {
+  return (
+    <PageLoading title="Loading Article" subtitle="Fetching blog content..." />
+  );
+}
